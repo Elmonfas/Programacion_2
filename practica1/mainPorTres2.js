2e4f2ecce11fee729d4aafb4c74134b06e0e3fc5
Monfas <pedromonfort075@gmail.com>
2025-03-05 19:44:49 +0100

    Practica 1 terminada por completo y corregida

--- practica1/mainPorTres2.js
+++ practica1/mainPorTres2.js
@@ -4,20 +4,24 @@
 
 // n:R -> porTres(x) -> R
 function porTres(n) {
-
-    return n*3
-
+    return n * 3; // Directamente devuelve el resultado
 }
 
+// Función asincrónica que espera 3s antes de devolver el resultado
+function porTresEspera(n, callback) {
+    setTimeout(() => {
+        const res = n * 3;
+        callback(res);
+    }, 3000);
+}
 
 // -----------------------------------
 
-let num = 5
+console.log(porTres(5)); // Imprime 15
 
-let res = porTres(num)
-
-setTimeout(function(){console.log(res)}, 3000)
-
+porTresEspera(3, (resultado) => {
+    console.log(resultado); // Esto imprimirá 9 después de 3 segundos
+});
 
 // -----------------------------------
 // -----------------------------------
